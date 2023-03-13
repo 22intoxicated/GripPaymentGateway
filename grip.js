d66291099a62974061c77844928d6369fd65f166
Sonali purwar <sonali@Sonalis-MacBook-Air.local>
2023-03-13 16:39:31 +0530

    First draft

---
+++ grip.js
@@ -0,0 +1,10 @@
+// const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+// const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl,{html:true}))
+
+function paymentGateway (){
+    window.open("payment.html", "_blank");
+}
+
+function goHome (){
+    window.location.href="index.html";
+}
